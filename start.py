2dcd29b6f1e7f3ad91294991b0d4bb72c372fdee
Justas <Justas@s-MacBook-Pro.local>
2022-02-02 12:32:09 +0200

    WIP

--- start.py
+++ start.py
@@ -1,7 +1,5 @@
 import uvicorn
 from app.main import app
 
-# async def read_items(commons: CommonQueryParams = Depends()):
-
 if __name__ == "__main__":
     uvicorn.run("start:app", host="0.0.0.0", port=8000, reload=True)
